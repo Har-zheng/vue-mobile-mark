a7330d2b84089244d26fb5762e2ca97fa885866d
Your Name <you@example.com>
2018-09-12 16:43:17 +0800

    add icons

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,17 @@
 <template>
   <div id="app">
-    <router-view/>
+    <Home></Home>
   </div>
 </template>
+<script>
+import Home from './pages/home/Home'
+  export default{
+    name: "App",
+    components:{
+      Home
+    }
+  }
+</script>
 <style lang="stylus">
 #app
   font-family 'Avenir', Helvetica, Arial, sans-serif
